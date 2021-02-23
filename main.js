f8136aab8f6e29a7019e3e06b06470166aace164
Riccco <32475423+Riccco@users.noreply.github.com>
2021-02-23 16:07:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
-function requset(data){
-    // 设置默认url
-    const url  = 'https://api.apishop.net/common/dogFamily/'
-
+const $xhrurl = 'www.baidu.com/'
+function xhr(url,{data=null,method='POST'} = {}){
+    console.log(data)
     return new Promise((resolve,reject)=>{
-        let xhr = new XMLHttpRequest(); // 建立请求
-        // const url  = 'https://api.apishop.net/common/dogFamily/'
-        // xhr.setRequestHeader('') 请求头 目前暂不设置
-        xhr.open(data.method,url+data.url) // 设置请求方式 请求地址 第三个参数默认异步 忽略
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-               resolve(xhr.response) //返回的数据体
-            }else if(xhr.status != 200){
-                reject('本次状态请求失败')
+        let $xhr = new XMLHttpRequest()
+        $xhr.open(method,$xhrurl+url) // 最后一个参数 是否异步 通常情况是异步，不需要操作
+        $xhr.onreadystatechange = function(){
+            if($xhr.readyState === 4){
+                if($xhr.status >= 200 || $xhr.status <= 300 || $xhr.status === 304){
+                    resolve($xhr.response)
+                }else{
+                    $xhr.abort()
+                    console.log('出错了')
+                }
+            }else{
+                reject($xhr)
             }
+
         }
-
-        // 请求的内容，如果是GET请求则传空
-        xhr.send(data.XML)
-
+        // 最后设置一下token
+        $xhr.setRequestHeader('token' , 'xxxxxxxx')
+        $xhr.send(data)
     })
 }
-
-const data = {
-    method:'GET',
-    url:'接收一个url',
-    XML:'',
-}
-
-requset(data)
-.then(res=>{
-    return res
-})
-.catch(res=>{
-    return res
-})
 
 
 
